8c026c9c797623799ba5bd57ee8e7c71c5b981ca
mariofredo <mariotannuwijaya@gmail.com>
2022-05-24 14:39:36 +0700

    add models and migration

---
+++ migrations/20220524071327-create-chef.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Chefs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Chefs');
+  }
+};
---
+++ migrations/20220524071846-create-order.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Orders", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ChefId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Chefs",
+          key: "id",
+        },
+      },
+      totalPrice: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Orders");
+  },
+};
