8e4622a10e3d7e14f060c091b1eefe5d404d4056
begawandiaz <begawandiazafa@gmail.com>
2022-05-24 23:12:53 +0700

    test regist feature

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,11 +1,36 @@
 const {User} = require('../models')
 const ValorantAPI = require("unofficial-valorant-api")
+
 class UserController {
   static async register(req,res,next) {
     try {
+      const {username,email,password,ign,tagline} = req.body
+
+      if (!ign) {
+        throw new Error("ign_required")
+      }
       
+      if(!tagline) {
+        throw new Error("tagline_required")
+      }
+
+      const {data} = await ValorantAPI.getAccount(ign,tagline)
+
+      if(!data.puuid) {
+        throw new Error("valo_account_not_found")
+      }
+
+      const puuid = data.puuid
+
+      const newUser = await User.create({username,email,password,puuid})
+
+      res.status(201).json({
+        id : newUser.id,
+        username : newUser.username,
+        puuid : newUser.puuid
+      })
     } catch (err) {
-      
+      next(err)
     }
   }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const usersRouter = require('./users')
+const valodatasRouter = require('./valodatas')
 
 router.use('/users', usersRouter)
 router.use('/valodatas',valodatasRouter)
