b7a8b776495c9cc69bc125298ed50d51e5a1099b
Audy Wallace <hwksin7@gmail.com>
2022-05-24 19:05:14 +0700

    delete all for firebase setup

--- helpers/encryption.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const hashPassword = (password) => bcrypt.hashSync(password);
-const comparePassword = (password, hashedPassword) => bcrypt.compareSync(password, hashedPassword);
-
-module.exports = {hashPassword: hashPassword, comparePassword: comparePassword}
