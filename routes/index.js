0661b4152479d79fc030cc6e215ff825d9d87af1
Kevin Samuel <kevinsbl3nd@gmail.com>
2022-05-24 16:06:57 +0700

    Created routes for login & register

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 "use strict"
 
 const express = require("express")
+const Controller = require("../controllers")
 const router = express.Router()
 const errorHandlres = require("../middlewares/errorHandlers")
 
-router.post("/register", (req, res) => {
-    res.send("login")
-})
-router.post("/login", (req, res) => {
-    res.send("login")
-})
+router.post("/register", Controller.register)
+router.post("/login", Controller.login)
 
 router.use(errorHandlres)
 
