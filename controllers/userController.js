11d86903187e571f4ee62affd03bfca3b62405c7
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-05-24 15:59:58 +0700

    add login feature for customer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,34 @@
+const { User } = require("../models");
+const { comparePassword, signToken } = require("../helpers");
+
 class UserController {
-    
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        throw { name: "EMAIL_OR_PASSWORD_NOT_FOUND" };
+      }
+
+      const isTruePassword = comparePassword(password, customer.password);
+
+      if (!isTruePassword) {
+        throw { name: "EMAIL_OR_PASSWORD_NOT_FOUND" };
+      }
+
+      const token = signToken({
+        id: customer.id,
+        email: customer.email,
+      });
+
+      res.status(200).json({
+        access_token: token,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
-module.exports = UserController
+module.exports = UserController;
