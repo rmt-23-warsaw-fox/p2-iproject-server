05452cd39eeab03dc7799e78c3d0cb143274be01
Audy Wallace <hwksin7@gmail.com>
2022-05-24 12:07:43 +0700

    add helper for data encryption

---
+++ helpers/encryption.js
@@ -0,0 +1,6 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = (password) => bcrypt.hashSync(password);
+const comparePassword = (password, hashedPassword) => bcrypt.compareSync(password, hashedPassword);
+
+module.exports = {hashPassword: hashPassword, comparePassword: comparePassword}
