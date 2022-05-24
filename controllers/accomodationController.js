03eecdab89e44212d6bb3cc776c9b32a90d98f36
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-05-24 15:48:54 +0700

    add feature fetch all accomodations

--- controllers/accomodationController.js
+++ controllers/accomodationController.js
@@ -1,12 +1,13 @@
-const { Accomodation, Admin } = require('../models');
-const { verifyToken } = require('../helpers');
+const { Accomodation, Type, Admin } = require("../models");
+const { verifyToken } = require("../helpers");
 
 class AccomodationController {
   static async create(req, res, next) {
     try {
       const { access_token } = req.headers;
-      const payload =  verifyToken(access_token);
-      let { name, facility, roomCapacity, imageUrl, location, price, TypeId } = req.body;
+      const payload = verifyToken(access_token);
+      let { name, facility, roomCapacity, imageUrl, location, price, TypeId } =
+        req.body;
       price = +price;
       roomCapacity = +roomCapacity;
       await Accomodation.create({
@@ -17,14 +18,35 @@ class AccomodationController {
         location,
         price,
         TypeId,
-        AdminId: payload.id
+        AdminId: payload.id,
       });
 
       res.status(201).json({
         message: `Adding new accomodation successfully`,
       });
     } catch (err) {
-        console.log(err);
+      console.log(err);
+      next(err);
+    }
+  }
+
+  static async fetchAllAccomodations(req, res, next) {
+    try {
+      const accomodations = await Accomodation.findAll({
+        include: [
+          {
+            model: Type,
+            attributes: ["name"],
+          },
+          {
+            model: Admin,
+            attributes: ["email", "firstName", "lastName"],
+          },
+        ],
+      });
+
+      res.status(200).json(accomodations);
+    } catch (err) {
       next(err);
     }
   }
