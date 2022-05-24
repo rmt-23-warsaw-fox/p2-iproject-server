a3eb61ee67677c63f3d31f60560acad08712468c
redyRenno <redyrenno@gmail.com>
2022-05-25 00:44:07 +0900

    create db

--- server/app.js
+++ server/app.js
@@ -1,21 +1,10 @@
 const express = require('express')
 const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const {Server} = require('socket.io')
-const io = new Server(server)
 
 const port = 3000
 
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.html')
-})
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnect');
-  })
+  response.send('hallo')
 })
 
 server.listen(port, () => {
