4ad691ef3fcbf70c60047954f31133389716431d
mariofredo <mariotannuwijaya@gmail.com>
2022-05-25 00:26:50 +0700

    add xendit route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("../middlewares/errHandler");
 const public = require("./public");
+const xendit = require("./xendit");
 const router = express.Router();
 
 router.use("/public", public);
+router.use("/xendit", xendit);
 router.use(errorHandler);
 module.exports = router;
---
+++ routes/xendit.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const xendit = express.Router();
+const axios = require("axios");
+const isLogin = require("../middlewares/authn");
+// const Xendit = require("xendit-node");
+// const x = new Xendit({
+//   secretKey:
+//     "xnd_development_m7B4Ljt4NfM75gfefg9rkgMoPg9ERQTurFWogGlhgaDJPEs44CcFNlyphBxfT",
+// });
+// const { VirtualAcc } = x;
+// const vaSpecificOptions = {};
+// const va = new VirtualAcc(vaSpecificOptions);
+xendit.use(isLogin);
+xendit.post("/create", async function (req, res, next) {
+  try {
+    const { bank_code, totalPrice } = req.body;
+    const { Username } = req.dataUser;
+    const response = await axios({
+      method: "post",
+      url: "https://api.xendit.co/callback_virtual_accounts",
+      auth: {
+        username:
+          "xnd_development_m7B4Ljt4NfM75gfefg9rkgMoPg9ERQTurFWogGlhgaDJPEs44CcFNlyphBxfT",
+        password: "Mario.fredo2",
+      },
+      data: {
+        external_id: `VA-fixed-${Date.now()}`,
+        bank_code,
+        name: Username,
+        suggested_amount: totalPrice,
+        expected_amount: totalPrice,
+        is_single_use: true,
+      },
+    });
+    res.status(200).json({
+      data: response.data,
+    });
+
+    // const fixedAcc = await va.createFixedVA({
+    //   externalID: `VA-fixed-${Date.now()}`,
+    //   bankCode: bank_code,
+    //   name: Username,
+    //   suggestedAmt: totalPrice,
+    //   expectedAmt: totalPrice,
+    //   isSingleUse: true,
+    // });
+    // res.status(200).json(fixedAcc);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = xendit;
