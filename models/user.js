4f8b10230c0dd39de005a0d30bfa69467581f5b9
Romzi Fauzi <romziflubis@gmail.com>
2022-05-24 15:25:43 +0700

    add allownull with value of false in model password property

---
+++ models/user.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: { args: true, msg: 'Email already exists' },
+        validate: {
+          isEmail: { args: true, msg: 'Please enter a valid email address' },
+          notNull: { args: true, msg: 'Please enter an email address' },
+          notEmpty: { args: true, msg: 'Please enter an email address' },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { args: true, msg: 'Please enter a password' },
+          notEmpty: { args: true, msg: 'Please enter a password' },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+
+  return User;
+};
