112812a82ffb2b89019a3fd12357b1dba7239ded
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-05-24 19:43:27 +0700

    add feature add accomodation to wishlist

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,5 +1,25 @@
+const { Wishlist, Accomodation } = require('../models');
+
 class WishlistController {
-    
+    static async addToWishlist(req, res, next) {
+        try {
+            const { AccomodationId, UserId } = req.body;
+            const [accomodation, created] = await Wishlist.findOrCreate({
+                where: { AccomodationId, UserId },
+            });
+
+            if (!created) {
+                throw { name: "ACCOMODATION_ALREADY_EXIST" }
+            }
+
+            res.status(201).json({
+                message: `Successfully added to wishlist`
+            })
+        } catch (err) {
+            console.log(err);
+            next(err)
+        }
+    }
 }
 
 module.exports = WishlistController
--- routers/index.js
+++ routers/index.js
@@ -6,14 +6,14 @@ const typeRoutes = require('./typeRoutes');
 const wishlistRoutes = require('./wishlistRoutes');
 const accomodationUserRoutes = require('./accomodationUserRoutes');
 
-const { authnUser } = require('../middlewares/authnUser');
+const authnUser = require('../middlewares/authnUser');
 
 routes.use("/admin", adminRoutes);
 routes.use("/accomodation", accomodationRoutes);
 routes.use("/type", typeRoutes);
 
 routes.use("/public/user", userRoutes)
-// routes.use("/public/wishlist", authnUser, wishlistRoutes)
+routes.use("/public/wishlist",authnUser, wishlistRoutes)
 routes.use("/public/accomodation", accomodationUserRoutes);
 
 module.exports = routes;
