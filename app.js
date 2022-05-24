b5ad119c4c2b4f33fd2e39116f7b6c76d400e7ae
Audy Wallace <hwksin7@gmail.com>
2022-05-24 19:10:21 +0700

    setup for firebase development

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+module.exports = express();
