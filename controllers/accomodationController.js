3bf8657afbcdbc15cecf5656d6617eecd0450a3f
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-05-24 15:40:21 +0700

    add feature create accomodation

--- controllers/accomodationController.js
+++ controllers/accomodationController.js
@@ -1,5 +1,33 @@
+const { Accomodation, Admin } = require('../models');
+const { verifyToken } = require('../helpers');
+
 class AccomodationController {
-    
+  static async create(req, res, next) {
+    try {
+      const { access_token } = req.headers;
+      const payload =  verifyToken(access_token);
+      let { name, facility, roomCapacity, imageUrl, location, price, TypeId } = req.body;
+      price = +price;
+      roomCapacity = +roomCapacity;
+      await Accomodation.create({
+        name,
+        facility,
+        roomCapacity,
+        imageUrl,
+        location,
+        price,
+        TypeId,
+        AdminId: payload.id
+      });
+
+      res.status(201).json({
+        message: `Adding new accomodation successfully`,
+      });
+    } catch (err) {
+        console.log(err);
+      next(err);
+    }
+  }
 }
 
-module.exports = AccomodationController
+module.exports = AccomodationController;
