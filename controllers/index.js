5625598efd163b08004276fd003b0170b5bf3a86
Kevin Samuel <kevinsbl3nd@gmail.com>
2022-05-24 15:57:37 +0700

    Feat:Created user login by email/username

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 "use strict"
 
 const { User } = require("../models")
-
+const { Op } = require("sequelize")
+const { hashCompare } = require("../helpers/bcrypts")
+const { createToken } = require("../helpers/jsonwebtoken")
 class Controller {
     static async register(req, res, next){
         try {
@@ -15,6 +17,41 @@ class Controller {
             res.status(201).json({
                 message:"Succes Register Account",
                 createUser
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async login (req, res, next){
+        try {
+            const { userName, email, password } = req.body
+            const user = await User.findOne({
+                where:{
+                    [Op.or]:[{userName}, {email}]
+                }
+            })
+            if(!user){
+                throw new Error ("Invalid login, please check your input!")
+            }
+
+            const correctPassword = hashCompare(password, user.password)
+
+            if(!correctPassword){
+                throw new Error("Invalid Password!")
+            }
+
+            const readPayLoad = {
+                id:user.id,
+                userName:user.id,
+                email:user.email
+            }
+
+            const access_token = createToken(readPayLoad)
+
+            res.status(200).json({
+                message:"Login Success",
+                access_token: access_token
             })
         } catch (err) {
             next(err)
--- middlewares/errorHandlers.js
+++ middlewares/errorHandlers.js
@@ -1,22 +1,32 @@
-"use strict"
-const errorHandlres = async(err, req, res, next) =>{
-    if(err.name === "SequelizeValidationError" || err.name === "SequelizeUniqueConstraintError"){
-        const msg = err.errors.map((e) =>{
-            return{
-                message:e.message
-            }
-        })
-        res.status(400).json({
-            message:msg
-        })
-    }  else {
-        res.status(500).json({
-            statusCode: 500,
-            error: {
-                message: "Internal Server Error",
-            },
-        });
-    }
-}
+"use strict";
+const errorHandlres = async (err, req, res, next) => {
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    const msg = err.errors.map((e) => {
+      return {
+        message: e.message,
+      };
+    });
+    res.status(400).json({
+      message: msg,
+    });
+  } else if (
+    err.message === "Invalid login, please check your input!" ||
+    err.message === "Invalid Password!"
+  ) {
+    res.status(401).json({
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      statusCode: 500,
+      error: {
+        message: "Internal Server Error",
+      },
+    });
+  }
+};
 
-module.exports = errorHandlres
+module.exports = errorHandlres;
