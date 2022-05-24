6089b15d383e9b29e25599c5938c7310d36d2755
Romzi Fauzi <romziflubis@gmail.com>
2022-05-24 23:42:23 +0700

    add payment functionality, create paymnet routes and controller, succesfully hitted with postmen

---
+++ controllers/payment.js
@@ -0,0 +1,51 @@
+const { User } = require('../models');
+const midtransClient = require('midtrans-client');
+// Create Snap API instance
+let snap = new midtransClient.Snap({
+  isProduction: false,
+  serverKey: 'SB-Mid-server-wbLcGSE8HpMB_os5vYzbrJgT',
+  clientKey: 'SB-Mid-client-PWi2oSB5f0ZMGyhH',
+});
+class ControllerPayment {
+  static async transaction(req, res, next) {
+    try {
+      //   const { id, email } = req.loginfo;
+
+      //   await User.update(
+      //     { isPremium: true },
+      //     {
+      //       where: {
+      //         id: id,
+      //       },
+      //     }
+      //   );
+
+      const transactionCode = `TRX${Math.floor(Math.random() * 1000000)}`;
+
+      let parameter = {
+        transaction_details: {
+          order_id: `${Date.now()}-${transactionCode}`,
+          gross_amount: '1000000',
+        },
+      };
+
+      const transaction = await snap.createTransaction(parameter);
+
+      let transactionToken = transaction.token;
+      console.log('transactionToken:', transactionToken);
+
+      let transactionRedirectUrl = transaction.redirect_url;
+      console.log('transactionRedirectUrl:', transactionRedirectUrl);
+
+      res.status(200).json({
+        token: transactionToken,
+        redirect_url: transactionRedirectUrl,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+}
+
+module.exports = ControllerPayment;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let router = express.Router();
 const users = require('./users');
+const payment = require('./payment');
 
-router.use('/user', users);
+router.use('/users', users);
+router.use('/payment', payment);
 
 module.exports = router;
