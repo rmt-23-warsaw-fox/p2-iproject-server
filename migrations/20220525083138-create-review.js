4cf3e3ff39c15b7641510fc758b8a7e589198bf5
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-05-25 21:40:12 +0700

    Login and register created

---
+++ migrations/20220525083138-create-review.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "cascade",
+        onUpdate: "cascade"
+      },
+      MovieId: {
+        type: Sequelize.INTEGER
+      },
+      review: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
