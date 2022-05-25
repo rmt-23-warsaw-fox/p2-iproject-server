b69b719fe76d7873abdff5ddbd33ac8d892cc4ce
redyRenno <redyrenno@gmail.com>
2022-05-25 14:05:25 +0900

    init cors urlencoded

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const port = 3000
 
+app.use(express.urlencoded({extended:false}));
+app.use(cors())
+
 app.get('/', (request, response) => {
   response.send('hallo')
 })
+
+app.post('/login')
 
 server.listen(port, () => {
   console.log('Listening on port :', port);
