6fe23be38c96e73a09891391bb959fbd2fc18727
redyRenno <redyrenno@gmail.com>
2022-05-25 15:52:23 +0900

    fetch api - memes - from rapid api - Programming Memes Images

--- server/app.js
+++ server/app.js
@@ -1,23 +1,38 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const {User} = require('./models/index')
-const {compareHash} = require('./helpers/bcrypt')
-const {createToken, decodeToken} = require('./helpers/jwt')
+const { User } = require('./models/index')
+const { compareHash } = require('./helpers/bcrypt')
+const { createToken, decodeToken } = require('./helpers/jwt')
 const axios = require("axios");
 
 const port = 3000
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.send('hallo')
+app.get('/memes', (request, response) => {
+  const options = {
+    method: 'GET',
+    url: 'https://programming-memes-images.p.rapidapi.com/v1/memes',
+    headers: {
+      'X-RapidAPI-Host': 'programming-memes-images.p.rapidapi.com',
+      'X-RapidAPI-Key': 'f6921824a3msh0f20574e869bd8bp1cd505jsn7289a82fe4c3'
+    }
+  };
+
+  axios.request(options).then(function (res) {
+    response.status(200).json({
+      memes: res.data
+    })
+  }).catch(function (error) {
+    console.error(error);
+  });
 })
 
 app.post('/login', async (request, response, next) => {
   try {
-    const {email, password} = request.body
+    const { email, password } = request.body
 
     const foundUser = await User.findOne({
       where: {
@@ -34,7 +49,7 @@ app.post('/login', async (request, response, next) => {
     if (isValidPassword === false) {
       throw new Error('unauthorized')
     }
-   
+
     let payload = {
       id: foundUser.id,
       username: foundUser.username,
