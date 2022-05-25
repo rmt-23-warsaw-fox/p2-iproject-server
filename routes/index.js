5e6421f6d6f2cf8936fa1ec8fe2f977a97df3c8d
aegisfiveniner <aegis.five.niner@gmail.com>
2022-05-25 14:47:32 +0700

    feat: added CRUD on product entity

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const user = require("./user");
+const product = require("./product");
 const { errorHandler } = require("../middlewares/errorHandler");
 
 // route to user
-router.use("/", user);
+router.use("/users", user);
+// route to product
+router.use("/products", product);
 
 router.use(errorHandler);
 
