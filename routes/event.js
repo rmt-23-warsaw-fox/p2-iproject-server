f908f2a8456bf0dce2a5a03f29d07637101fb6de
WibuSyariah <mspa43@gmail.com>
2022-05-25 15:27:11 +0700

    /events Routes: GET /, GET /master-list, PATCH /hands/:EventId feature added

--- routes/event.js
+++ routes/event.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const eventController = require("../controllers/eventController");
+const eventController = require("../controllers/eventController");
 const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
 
 router.get("/", eventController.eventList);
 router.get("/master-list", eventController.eventMasterList);
-router.patch("/hands/:EventId", authentication, eventController.patchHands);
+router.patch(
+  "/hands/:EventId",
+  authentication,
+  authorization,
+  eventController.patchHands
+);
 
 module.exports = router;
