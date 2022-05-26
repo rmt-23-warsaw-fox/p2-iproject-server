feef19b808b6ff6deb46877be5764af14adcf197
redyRenno <redyrenno@gmail.com>
2022-05-26 17:45:47 +0900

    setting validation and associations

--- server/models/post.js
+++ server/models/post.js
@@ -11,12 +11,25 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Post.belongsTo(models.User)
     }
   }
   Post.init({
-    imgUrl: DataTypes.STRING,
+    imgUrl: {
+      type: DataTypes.STRING,
+      allowNull: {args: false, msg: 'Image URL cannot be null.'},
+      validate: {
+        notEmpty: {args: true, msg: 'Image URL is required.'}
+      }
+    },
     description: DataTypes.STRING,
-    UserId: DataTypes.INTEGER
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: {args: false, msg: 'UserId cannot be null.'},
+      validate: {
+        notEmpty: {args: true, msg: 'UserId is required.'}
+      }
+    }
   }, {
     sequelize,
     modelName: 'Post',
