5888cea296e1f6bb409717734e71d967fb326ddf
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-05-26 08:40:06 +0700

    Added Review and Authenticators Features

---
+++ controllers/reviewController.js
@@ -0,0 +1,103 @@
+const { Review } = require("../models");
+
+class reviewController {
+  static async postReview(req, res, next) {
+    try {
+      const { review } = req.body;
+      const { MovieId } = req.params;
+
+      const postReeview = await Review.create({
+        UserId: +req.additionalData.id,
+        MovieId: +MovieId,
+        review,
+      });
+
+      res.status(201).json({
+        review: postReeview.review,
+      });
+    } catch (error) {
+      res.status(403).json(error);
+    }
+  }
+
+  static async getAllReview(req, res, next) {
+    try {
+      const allReviews = await Review.findAll();
+
+      res.status(200).json(allReviews);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  static async getReviewUser(req, res, next) {
+    try {
+      const { id } = req.params;
+      const allReviews = await Review.findAll({
+        where: {
+          id,
+        },
+      });
+
+      res.status(200).json(allReviews);
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  static async editReview(req, res, next) {
+    try {
+      const { MovieId } = req.params;
+      const { review } = req.body;
+
+      const reviewPost = await Review.update(
+        {
+          review,
+        },
+        {
+          where: {
+            MovieId,
+          },
+        }
+      );
+
+      res.status(201).json(reviewPost);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async reviewDetail(req, res, next) {
+    try {
+      const { id } = req.params;
+      const review = await Review.findById(id);
+
+      if (!review) {
+        throw new Error({ name: "review not found", message: "Review not found" });
+      }
+      res.status(200).json({ review });
+    } catch (error) {
+      res.status(404).json({ error });
+    }
+  }
+
+  static async deleteReview(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const deletedReview = await Review.destroy({
+        where: {
+          id: +id,
+        },
+      });
+
+      if (!deletedReview) {
+        throw new Error({ name: "review not found", message: "Review not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+}
+
+module.exports = reviewController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const reviewController = require("../controllers/reviewController");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const authN = require("../middlewares/authN");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+
+router.get("/reviews", reviewController.getAllReview);
+
+router.use(authN);
+
+router.get("/reviews/user/:id", reviewController.getReviewUser);
+router.post("/reviews/add/:MovieId", reviewController.postReview);
+router.get("/reviews/detail/:id", reviewController.reviewDetail);
+router.patch("/reviews/edit/:MovieId", reviewController.editReview);
+router.delete("/reviews/:id", reviewController.deleteReview);
 
 module.exports = router;
