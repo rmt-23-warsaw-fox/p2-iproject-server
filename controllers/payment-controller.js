7e8f8cb17637400ff61318575075d22b89b61b05
FransYosua <snarf.frans7@gmail.com>
2022-05-26 18:55:23 +0700

    add feature payment

--- controllers/payment-controller.js
+++ controllers/payment-controller.js
@@ -1,25 +1,27 @@
-const axios = require("axios");
 const snap = require("../axios/midtrans");
+const { TransactionHistory } = require("../models");
 
 class Controller {
   static async createPayment(req, res, next) {
     try {
+      const { firstName, lastName, email, phone, priceTotal } = req.body;
+
       const dateId = new Date().toJSON().slice(0, 10).replace(/-/g, "");
-      const orderId = `${dateId}${Math.floor(Math.random() * 10000)}`;
+      const orderId = `iProject-${dateId}${Math.floor(Math.random() * 10000)}`;
 
       let parameter = {
         transaction_details: {
-          order_id: `iProject-${orderId}`,
-          gross_amount: req.body.price,
+          order_id: orderId,
+          gross_amount: priceTotal,
         },
         credit_card: {
           secure: true,
         },
         customer_details: {
-          first_name: "user",
-          last_name: "dummy",
-          email: req.user.email,
-          phone: "78111222333",
+          first_name: firstName,
+          last_name: lastName,
+          email: email,
+          phone: phone,
         },
       };
 
@@ -29,8 +31,42 @@ class Controller {
 
       res.status(201).json({
         message: "Payment created successfully",
-        token: transactionToken,
+        orderId,
+        orderToken: transactionToken,
         redirect_url: transactionRedirectUrl,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  static async successPayment(req, res, next) {
+    try {
+      const userId = req.user.id;
+      const {
+        brand,
+        productName,
+        price,
+        amount,
+        priceTotal,
+        detailProduct,
+        orderId,
+      } = req.body;
+
+      const data = await TransactionHistory.create({
+        userId,
+        brand,
+        productName,
+        price,
+        amount,
+        priceTotal,
+        detailProduct,
+        orderId,
+      });
+
+      res.status(200).json({
+        message: "Payment success",
       });
     } catch (error) {
       console.log(error);
