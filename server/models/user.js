3b662722a9098a5eca7fa81f73d732963de2890b
redyRenno <redyrenno@gmail.com>
2022-05-26 17:35:59 +0900

    migration Post

--- server/models/user.js
+++ server/models/user.js
@@ -14,9 +14,45 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Username cannot be null.'
+      },
+      unique: {
+        args: true,
+        msg: 'Username already exist.'
+      },
+      validate: {
+        notEmpty: {args: true, msg: 'Username is required.'}
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Email cannot be null.'
+      },
+      unique: {
+        args: true,
+        msg: 'Email already exist.'
+      },
+      validate: {
+        notEmpty: {args: true, msg: 'Email is required.'},
+        isEmail: {args: true, msg: 'Email format is required.'}
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Password cannot be null.'
+      },
+      validate: {
+        notEmpty: {args: true, msg: 'Password is required.'}
+      }
+    },
     gender: DataTypes.STRING,
     bio: DataTypes.STRING
   }, {
