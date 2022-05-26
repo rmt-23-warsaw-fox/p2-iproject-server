5d686cb1e80378ed554b7d628a753412555cde61
dendijoil <dendijoil@gmail.com>
2022-05-26 10:33:13 +0800

    Add env example and appjs

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3000
+const axios = require('axios')
+const routes = require('./routes/index')
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+})
