79c2d5c63ae149e041cc34375bbcf3c43782e58b
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-05-26 23:21:25 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 const express = require("express");
 const cors = require("cors");
 const app = express();
