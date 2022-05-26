3c7fee57d2e4e89bfb1928a586c472cc01276e43
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-05-26 20:01:12 +0700

    delete route add-transaction

--- routers/wishlistRoutes.js
+++ routers/wishlistRoutes.js
@@ -4,7 +4,6 @@ const WishlistController = require('../controllers/wishlistController');
 wishlistRoutes.get('/', WishlistController.fetchWishlist);
 wishlistRoutes.post('/add', WishlistController.addToWishlist);
 wishlistRoutes.post("/payment", WishlistController.payment)
-wishlistRoutes.post("/add-transaction", WishlistController.addTransaction)
 wishlistRoutes.get("/transactions", WishlistController.fetchTransactions)
 wishlistRoutes.post('/payment-status', WishlistController.updatePaymentStatus)
 wishlistRoutes.delete('/:id', WishlistController.deleteWishlist);
