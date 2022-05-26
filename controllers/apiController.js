ccee6a0d9727bed23e293d2d05a8e7fdefe0a471
Afghani Trisna Ramadhan <100565301+afghanitr@users.noreply.github.com>
2022-05-26 20:06:46 +0700

    Added Movie Detail and Get Trending Features

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,15 +3,17 @@ const axios = require("axios");
 class apiController {
   static async searchMovie(req, res, next) {
     try {
-      const api_key = "8cfeb5ca09af2ee8c4cec15101a61248";
-      const { query } = req.query;
+      const api_key = process.env.API_KEY;
+      const { query, page } = req.query;
+
+      if (!page) {
+        page = 1;
+      }
 
       const { data } = await axios({
         method: "get",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`,
+        url: `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}&page=${page}`,
       });
-
-      console.log(data);
       res.status(200).json(data);
     } catch (error) {
       res.status(500).json(error);
@@ -20,11 +22,33 @@ class apiController {
 
   static async movieDetail(req, res, next) {
     try {
-      
+      const { MovieId } = req.params;
+      const api_key = process.env.API_KEY;
+
+      const { data } = await axios({
+        method: "get",
+        url: `https://api.themoviedb.org/3/movie/${MovieId}?api_key=${api_key}`,
+      });
+      res.status(200).json(data);
     } catch (error) {
       res.status(500).json(error);
     }
-  };
+  }
+
+  static async getTrending(req, res, next) {
+    try {
+      const api_key = process.env.API_KEY;
+      const { page } = req.query;
+
+      const { data } = await axios({
+        method: "get",
+        url: `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}&page=${page}`,
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
 
 module.exports = apiController;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,5 +3,7 @@ const apiController = require("../controllers/apiController");
 const router = express.Router();
 
 router.get("/search", apiController.searchMovie)
+router.get("/movie-detail/:MovieId", apiController.movieDetail)
+router.get("/movie-trending", apiController.getTrending)
 
 module.exports = router;
