5b35e91c20d29b54d8e22637f7a0fdb129b57255
josuawilliams <josuawilliams17@gmail.com>
2022-05-26 22:02:50 +0700

    Deploy Heroku 0.0.0

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 "use strict";
-require('dotenv').config()
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config()
+}
 const express = require('express')
 const app = express()
 const errorHandle = require('./middleware/handleError')
 const cors = require('cors')
 const news = require('./route/newsroute')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
