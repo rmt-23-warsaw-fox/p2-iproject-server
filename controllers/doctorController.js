db229620bc9181ea22e30e69161f634735d5053b
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-05-26 21:25:40 +0700

    added myAppointments and approve

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,75 +1,106 @@
-const { toVerify } = require("../helpers/bcrypt");
-const { toToken } = require("../helpers/jwt");
+const { toVerify, compareHash } = require("../helpers/bcrypt");
+const { toToken, generateToken } = require("../helpers/jwt");
 const { Doctor, DoctorPatient, Patient } = require("../models");
 
 class doctorController {
-    static async login(req, res, next) {
-        console.log("Doctor login controller");
-        console.log(req.body.email, "<<<<<<<<<");
-    
-        try {
-          const response = await Doctor.findOne({
-            where: {
-              email: req.body.email,
-            },
-          });
-          if (!response) {
-            throw { name: "D_NOT_FOUND" };
-          } else {
-            const isValid = compareHash(req.body.password, response.password);
-            console.log(isValid, "<<<< isValid");
-            console.log(response.id, "<<<< response id");
-            if (!isValid) {
-              throw { name: "D_NOT_FOUND" };
-            }
-            const payload = {
-              id: response.id,
-            };
-            const token = generateToken(payload);
-    
-            res.status(200).json({
-              statusCode: 200,
-              access_token: token,
-              customer_id: response.id,
-              customer_email: response.email,
-            });
-          }
-        } catch (err) {
-          console.log(err);
-          next(err);
+  static async login(req, res, next) {
+    console.log("Doctor login controller");
+    console.log(req.body.email, "<<<<<<<<<");
+
+    try {
+      const response = await Doctor.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+      if (!response) {
+        throw { name: "D_NOT_FOUND" };
+      } else {
+        const isValid = compareHash(req.body.password, response.password);
+        console.log(isValid, "<<<< isValid");
+        console.log(response.id, "<<<< response id");
+        if (!isValid) {
+          throw { name: "D_NOT_FOUND" };
         }
+        const payload = {
+          id: response.id,
+        };
+        const token = generateToken(payload);
+
+        res.status(200).json({
+          statusCode: 200,
+          access_token: token,
+          doctor_id: response.id,
+          doctor_email: response.email,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+
+  static async myAppointments2(req, res, next) {
+    console.log("My appointments 2");
+    const { id } = req.user;
+    try {
+      const response = await DoctorPatient.findAll({
+        where: {
+          PatientId: id,
+        },
+        include: {
+          model: Patient,
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        },
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      res.status(200).json(response);
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+
+  static async Approve(req, res, next) {
+    const {approveId} = req.params;
+    const { id } = req.user;
+    const {patientId} = req.body
+    console.log(req.body)
+    console.log('approve id', approveId)
+    console.log('patientId', patientId)
+    try {
+      const FindPatient = await DoctorPatient.findByPk(patientId);
+      if (!FindPatient) {
+        throw { name: "Patient not found" };
       }
 
-    static async Approve(req, res, next){
-        const patientId = req.params.id;
-        const { id } = req.user;
-        try {
-          const FindPatient = await DoctorPatient.findByPk(patientId);
-          if (!FindPatient) {
-            throw { name: "Patient not found" };
-          }
-      
-          if (FindPatient.DoctorId !== id) {
-            throw { name: "You are not authorized" };
-          }
-      
-          await MyHero.update(
-            {
-              status: "Played",
-            },
-            {
-              where: { HeroId: heroId },
-            }
-          );
-      
-          res.status(200).json({
-            message: "Hero has been played",
-          });
-        } catch (err) {
-          console.log(err);
-          next(err);
+      console.log(FindPatient)
+
+      if (FindPatient.DoctorId !== id) {
+        throw { name: "You are not authorized" };
+      }
+
+      await DoctorPatient.update(
+        {
+          status: "approved",
+        },
+        {
+          where: { id: approveId },
         }
-      };
+      );
+
+      res.status(200).json({
+        message: "Approved",
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
 }
 
-module.exports = doctorController
+module.exports = doctorController;
--- routers/doctorRouter.js
+++ routers/doctorRouter.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const doctorController = require('../controllers/doctorController')
+const authentication = require('../middlewares/authentication')
 const doctorRouter = express.Router()
 
 // define the home page route
 doctorRouter.post('/login', doctorController.login)
+doctorRouter.use(authentication)
+doctorRouter.get('/myAppointments', doctorController.myAppointments2)
+doctorRouter.put('/approve/:approveId', doctorController.Approve)
 
 
 module.exports = doctorRouter
