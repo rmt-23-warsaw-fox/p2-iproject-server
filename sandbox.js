372f5f6a7c7ef347a42c763061a3ffb054b4cb66
aegisfiveniner <aegis.five.niner@gmail.com>
2022-05-27 07:57:26 +0700

    deploy heroku

--- sandbox.js
+++
@@ -1,8 +0,0 @@
-// let r = (Math.random() + 1).toString(36).substring(7);
-// console.log("random", r);
-
-const { verification } = require("./helpers/sib");
-
-const recipient = "nanemquan@outlook.com";
-const link = "www.google.com";
-verification(recipient, link);
