0998e33912d92e47463b374182ecc2721a8ef6ea
Fahri Avicienna <fahri.aviciennaa@gmail.com>
2022-05-27 15:43:42 +0700

    nodemailer

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -3,11 +3,27 @@
 const { User, Review } = require('../models');
 const { comparePassword } = require('../helpers/bcrypt');
 const { signToken } = require('../helpers/jwt');
+const nodemailer = require('nodemailer');
 
 class ControllerUser {
   static async register(req, res, next) {
     try {
       const { email, password, address, phoneNumber } = req.body;
+
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.email,
+          pass: process.env.passwordEmail,
+        },
+      });
+
+      var mailOptions = {
+        from: 'fahri.aviciennaa@gmail.com',
+        to: email,
+        subject: 'Thank you for registrating to our website',
+        text: `Start browsing the movie or anime that you wanna see the details~`,
+      };
 
       const newUser = await User.create({
         email,
@@ -15,12 +31,15 @@ class ControllerUser {
         address,
         phoneNumber,
       });
-      
+
+      const sender = await transporter.sendMail(mailOptions);
+
       res.status(201).json({
         id: newUser.id,
         email: newUser.email,
-      })
+      });
     } catch (err) {
+      console.log(err);
       next(err);
     }
   }
@@ -31,31 +50,31 @@ class ControllerUser {
 
       const foundUser = await User.findOne({
         where: {
-          email
-        }
-      })
+          email,
+        },
+      });
 
       if (!foundUser) {
-        throw new Error('USER_NOT_FOUND')
+        throw new Error('USER_NOT_FOUND');
       }
 
-      const isPassword = comparePassword(password, foundUser.password)
+      const isPassword = comparePassword(password, foundUser.password);
 
       if (!isPassword) {
-        throw new Error('INCORRECT_PASSWORD')
+        throw new Error('INCORRECT_PASSWORD');
       }
 
       const payload = {
         id: foundUser.id,
-        email: foundUser.email
-      }
+        email: foundUser.email,
+      };
 
-      const access_token = signToken(payload)
+      const access_token = signToken(payload);
 
       res.status(200).json({
         message: `You've logged in successfully`,
-        access_token
-      })
+        access_token,
+      });
     } catch (err) {
       console.log(err);
       next(err);
